931a761e085dada746bc59093e02fee515fd2b8b
kaipin2 <815sugisaki@gmail.com>
2022-03-25 23:25:46 +0900

    New Title_Slime.cs
    titleシーンにぞんざいしているス●イム（キャラ）を制御するスクリプト。
    内容としては、反時計回りをするプログラムである。

---
+++ Title_Slime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title_Slime : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = new Vector3(0, 1f, 0);
+    }
+}
